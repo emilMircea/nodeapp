1f45ace4fcc4e0aea5de1c4e4e4636adf63264cb
emilMircea <emil-mircea@protonmail.com>
2017-05-16 19:54:00 +0200

    adds edit and save functionality

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -14,3 +14,32 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Succesfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 }
+
+exports.getStores = async (req, res) => {
+  // query the db for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', {title: 'stores', stores: stores});
+}
+
+exports.editStore = async (req, res) => {
+  // find store by id
+  const store = await Store.findOne({_id: req.params.id})
+  res.render('editStore', {title: `Edit ${store.name}`, store: store});
+  // confirm owner of store to update
+
+  // render edit form
+}
+
+exports.updateStore  = async (req, res) => {
+  // find and update the store
+  const store = await Store.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    {
+      new: true, // return the new store instead of old one
+      runValidators: true
+    }
+  ).exec();
+  req.flash('success', `Succesfully updated <strong>${store.name}</strong>. <a href='/stores/${store.slug}'>View Store</a>`)
+  res.redirect(`/stores/${store._id}/edit`)
+}
